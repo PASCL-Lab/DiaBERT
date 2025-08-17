dd51fa77e167181e7f634d14c425bcb137b214cf
Linda Okpanachi <adamu.linda@gmail.com>
2025-08-16 21:58:58 -0700

    Add new Chrome extension files (new_files/)

---
+++ new_files/content-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('mouseup', () => {
+  const selectedText = window.getSelection().toString().trim();
+
+  if (selectedText.length > 0) {
+    chrome.storage.local.set({
+      diabertHighlightedText: selectedText
+    });
+  }
+});
